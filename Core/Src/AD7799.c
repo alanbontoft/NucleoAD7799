b6dbb27dd9ecf92317e31a47ae55807c4c71ea1e
Alan <alan.bontoft@gmail.com>
2022-05-28 21:43:47 +0100

    Bug fix in AD7799_SingleConversion
    cast to uint32_t now done on whole calculation, not just 1.5.
    
    Switches added and formatting of functions 'fixed'

--- Core/Src/AD7799.c
+++ Core/Src/AD7799.c
@@ -14,19 +14,19 @@
  * @param ad7799
  * @return HAL_OK or HAL_ERROR when something wrong
  */
-HAL_StatusTypeDef AD7799_Init(AD7799 *ad7799) {
-	if ((AD7799_GetRegisterValue(ad7799, AD7799_REG_ID, 1) & 0x0F) != AD7799_ID) {
-		return HAL_ERROR;
-	} else {
-		return HAL_OK;
-	}
+HAL_StatusTypeDef AD7799_Init(AD7799 *ad7799)
+{
+	if ((AD7799_GetRegisterValue(ad7799, AD7799_REG_ID, 1) & 0x0F) != AD7799_ID) return HAL_ERROR;
+
+	return HAL_OK;
 }
 
 /**
  * Reset Function. Clear all configuration
  * @param ad7799
  */
-void AD7799_Reset(AD7799 *ad7799) {
+void AD7799_Reset(AD7799 *ad7799)
+{
 	uint8_t dataToSend[4] = { 0xff, 0xff, 0xff, 0xff };
 	HAL_GPIO_WritePin(ad7799->CSPort, ad7799->CSPin, GPIO_PIN_RESET);
 	HAL_SPI_Transmit(ad7799->adcspi, dataToSend, 4, AD7799_TIMEOUT_SPI);
@@ -46,8 +46,8 @@ void AD7799_Reset(AD7799 *ad7799) {
  * @param size
  * @return register value in 4 bytes
  */
-uint32_t AD7799_GetRegisterValue(AD7799 *ad7799, uint8_t regAddress,
-		uint8_t size) {
+uint32_t AD7799_GetRegisterValue(AD7799 *ad7799, uint8_t regAddress, uint8_t size)
+{
 	uint8_t data[4] = { 0x00, 0x00, 0x00, 0x00 };
 	uint32_t receivedData = 0x00;
 	data[0] = AD7799_COMM_READ | AD7799_COMM_ADDR(regAddress);
@@ -55,18 +55,25 @@ uint32_t AD7799_GetRegisterValue(AD7799 *ad7799, uint8_t regAddress,
 	HAL_SPI_Transmit(ad7799->adcspi, data, 1, AD7799_TIMEOUT_SPI);
 	HAL_SPI_Receive(ad7799->adcspi, data, size, AD7799_TIMEOUT_SPI);
 	HAL_GPIO_WritePin(ad7799->CSPort, ad7799->CSPin, GPIO_PIN_SET);
-	if (size == 1) {
-		receivedData += (data[0] << 0);
+
+	switch (size)
+	{
+		case 1:
+			receivedData += (data[0] << 0);
+			break;
+		case 2:
+			receivedData += (data[0] << 8);
+			receivedData += (data[1] << 0);
+			break;
+		case 3:
+			receivedData += (data[0] << 16);
+			receivedData += (data[1] << 8);
+			receivedData += (data[2] << 0);
+			break;
+		default:
+			break;
 	}
-	if (size == 2) {
-		receivedData += (data[0] << 8);
-		receivedData += (data[1] << 0);
-	}
-	if (size == 3) {
-		receivedData += (data[0] << 16);
-		receivedData += (data[1] << 8);
-		receivedData += (data[2] << 0);
-	}
+
 	return receivedData;
 }
 
@@ -77,22 +84,29 @@ uint32_t AD7799_GetRegisterValue(AD7799 *ad7799, uint8_t regAddress,
  * @param regValue
  * @param size
  */
-void AD7799_SetRegisterValue(AD7799 *ad7799, uint8_t regAddress,
-		uint32_t regValue, uint8_t size) {
+void AD7799_SetRegisterValue(AD7799 *ad7799, uint8_t regAddress, uint32_t regValue, uint8_t size)
+{
 	uint8_t data[4] = { 0x00, 0x00, 0x00, 0x00 };
 	data[0] = AD7799_COMM_WRITE | AD7799_COMM_ADDR(regAddress);
-	if (size == 1) {
-		data[1] = (uint8_t) regValue;
+
+	switch (size)
+	{
+		case 1:
+			data[1] = (uint8_t) regValue;
+			break;
+		case 2:
+			data[2] = (uint8_t) ((regValue & 0x0000FF) >> 0);
+			data[1] = (uint8_t) ((regValue & 0x00FF00) >> 8);
+			break;
+		case 3:
+			data[3] = (uint8_t) ((regValue & 0x0000FF) >> 0);
+			data[2] = (uint8_t) ((regValue & 0x00FF00) >> 8);
+			data[1] = (uint8_t) ((regValue & 0xFF0000) >> 16);
+			break;
+		default:
+			break;
 	}
-	if (size == 2) {
-		data[2] = (uint8_t) ((regValue & 0x0000FF) >> 0);
-		data[1] = (uint8_t) ((regValue & 0x00FF00) >> 8);
-	}
-	if (size == 3) {
-		data[3] = (uint8_t) ((regValue & 0x0000FF) >> 0);
-		data[2] = (uint8_t) ((regValue & 0x00FF00) >> 8);
-		data[1] = (uint8_t) ((regValue & 0xFF0000) >> 16);
-	}
+
 	HAL_GPIO_WritePin(ad7799->CSPort, ad7799->CSPin, GPIO_PIN_RESET);
 	HAL_SPI_Transmit(ad7799->adcspi, data, 1 + size, AD7799_TIMEOUT_SPI);
 	HAL_GPIO_WritePin(ad7799->CSPort, ad7799->CSPin, GPIO_PIN_SET);
@@ -104,7 +118,8 @@ void AD7799_SetRegisterValue(AD7799 *ad7799, uint8_t regAddress,
  * @param ad7799
  * @return
  */
-uint8_t AD7799_Ready(AD7799 *ad7799) {
+uint8_t AD7799_Ready(AD7799 *ad7799)
+{
 	uint8_t rdy = 0;
 	rdy = (AD7799_GetRegisterValue(ad7799, AD7799_REG_STAT, 1) & 0x80);
 	return (!rdy);
@@ -115,7 +130,8 @@ uint8_t AD7799_Ready(AD7799 *ad7799) {
  * @param ad7799
  * @param mode
  */
-void AD7799_SetMode(AD7799 *ad7799, AD7799_Mode mode) {
+void AD7799_SetMode(AD7799 *ad7799, AD7799_Mode mode)
+{
 	uint32_t command;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_MODE, 2);
 	command &= ~AD7799_MODE_SEL(0xFF);
@@ -129,7 +145,8 @@ void AD7799_SetMode(AD7799 *ad7799, AD7799_Mode mode) {
  * @param ad7799
  * @param rate
  */
-void AD7799_SetRate(AD7799 *ad7799, AD7799_Rate rate) {
+void AD7799_SetRate(AD7799 *ad7799, AD7799_Rate rate)
+{
 	uint32_t command;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_MODE, 2);
 	command &= ~AD7799_MODE_RATE(0xFF);
@@ -143,7 +160,8 @@ void AD7799_SetRate(AD7799 *ad7799, AD7799_Rate rate) {
  * @param ad7799
  * @param channel
  */
-void AD7799_SetChannel(AD7799 *ad7799, AD7799_Channel channel) {
+void AD7799_SetChannel(AD7799 *ad7799, AD7799_Channel channel)
+{
 	uint32_t command;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_CONF, 2);
 	command &= ~AD7799_CONF_CHAN(0xFF);
@@ -157,7 +175,8 @@ void AD7799_SetChannel(AD7799 *ad7799, AD7799_Channel channel) {
  * @param ad7799
  * @param gain
  */
-void AD7799_SetGain(AD7799 *ad7799, AD7799_Gain gain) {
+void AD7799_SetGain(AD7799 *ad7799, AD7799_Gain gain)
+{
 	uint32_t command;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_CONF, 2);
 	command &= ~AD7799_CONF_GAIN(0xFF);
@@ -171,7 +190,8 @@ void AD7799_SetGain(AD7799 *ad7799, AD7799_Gain gain) {
  * @param ad7799
  * @param state
  */
-void AD7799_SetReference(AD7799 *ad7799, uint8_t state) {
+void AD7799_SetReference(AD7799 *ad7799, uint8_t state)
+{
 	uint32_t command = 0;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_CONF, 2);
 	command &= ~AD7799_CONF_REFDET(1);
@@ -184,7 +204,8 @@ void AD7799_SetReference(AD7799 *ad7799, uint8_t state) {
  * @param ad7799
  * @param polarity
  */
-void AD7799_SetPolarity(AD7799 *ad7799, AD7799_Polarity polarity) {
+void AD7799_SetPolarity(AD7799 *ad7799, AD7799_Polarity polarity)
+{
 	uint32_t command = 0;
 	command = AD7799_GetRegisterValue(ad7799, AD7799_REG_CONF, 2);
 	command &= ~AD7799_CONF_POLAR(1);
@@ -198,18 +219,18 @@ void AD7799_SetPolarity(AD7799 *ad7799, AD7799_Polarity polarity) {
  * @param ad7799
  * @return HAL_OK or HAL_TIMEOUT when conversion take too long
  */
-HAL_StatusTypeDef AD7799_SingleConversion(AD7799 *ad7799) {
+HAL_StatusTypeDef AD7799_SingleConversion(AD7799 *ad7799)
+{
 	AD7799_SetMode(ad7799, AD7799_MODE_SINGLE);
 
 	uint32_t startTime = HAL_GetTick();
-	while (!AD7799_Ready(ad7799)) {
-		if (HAL_GetTick() - startTime
-				> (uint32_t) 15.0 * settle_time_ms[(uint8_t) ad7799->rate]) {
+	while (!AD7799_Ready(ad7799))
+	{
+		if (HAL_GetTick() - startTime > (uint32_t) (1.5 * settle_time_ms[(uint8_t) ad7799->rate]))
+		{
 			return HAL_TIMEOUT;
 		}
 	}
-
-	// note above timeout was 1.5*
 
 	ad7799->rawConversion = AD7799_GetRegisterValue(ad7799, AD7799_REG_DATA, 3);
 
@@ -222,14 +243,18 @@ HAL_StatusTypeDef AD7799_SingleConversion(AD7799 *ad7799) {
  * Convert raw value to voltage
  * @param ad7799
  */
-void AD7799_RawToVolt(AD7799 *ad7799) {
+void AD7799_RawToVolt(AD7799 *ad7799)
+{
 	float gain = (float) pow(2.0, (double) ad7799->gain);
 
-	if (ad7799->polarity == AD7799_UNIPOLAR) {
+	if (ad7799->polarity == AD7799_UNIPOLAR)
+	{
 		float fullscale = (float) pow(2.0, (double) N);
 		ad7799->voltConversion = (float) ad7799->rawConversion
 				* (ad7799->vref / (fullscale * gain));
-	} else {
+	}
+	else
+	{
 		float fullscale = (float) pow(2.0, (double) (N - 1));
 		ad7799->voltConversion = (((float) ad7799->rawConversion - fullscale)
 				* ad7799->vref) / (fullscale * gain);
